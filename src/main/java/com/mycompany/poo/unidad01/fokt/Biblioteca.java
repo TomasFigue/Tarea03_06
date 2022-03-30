5f6912e8a395d5ddeedfa265aac764db58472571
PC-04 <PC-04@DESKTOP-9GUTMOK>
2022-03-30 14:46:13 +0200

    clase Biblioteca

---
+++ src/main/java/com/mycompany/poo/unidad01/fokt/Biblioteca.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo.unidad01.fokt;
+
+import java.util.Date;
+
+/**
+ *
+ * @author PC-04
+ */
+public class Biblioteca {
+    String autor;
+    Date fecha;
+    int nombreLibro;
+    double peso;
+    
+    
+}
